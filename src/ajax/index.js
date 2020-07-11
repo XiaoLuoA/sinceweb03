8e7852b6dfd1b4f135edce3448f5ffc9973b1f37
fuWenMr <nyfjr@icloud.com>
2020-07-11 14:20:28 +0800

    代码规范化

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -3,27 +3,12 @@ import {
   post,
   ajax,
 } from './ajax_axios';
-import {host} from "~/ajax/config";
+import { host } from '~/ajax/config';
 
 export const getIndex = (userName, password) => {
     return get('http://localhost:8081//page/index/index.html', { userName, password } );
 }
 
 export const doLogin = (userName, password) => {
-  return post(host+'/login', { userName, password } );
+  return post(host + '/login', { userName, password } );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/ajax/user.js
+++ src/ajax/user.js
@@ -1,12 +1,12 @@
-import {get, post} from "~/ajax/ajax_axios";
-import {host} from "~/ajax/config";
+import {get, post} from '~/ajax/ajax_axios';
+import {host} from '~/ajax/config';
 
 
 export const getUser = (open_id) => {
-  return get(host+'/user/findUserByOpenId', { open_id } );
+  return get(host + '/user/findUserByOpenId', { open_id } );
 }
 
 
 export const addUser = (wx_name, wx_image, wx_address, open_id) => {
-  return post(host+'/user/add',{wx_name, wx_image, wx_address, open_id})
+  return post(host + '/user/add', {wx_name, wx_image, wx_address, open_id})
 }
