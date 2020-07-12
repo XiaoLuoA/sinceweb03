86325f01068ba6523acdd669f4018b47794f38fa
luoxinyuan <1019558287@qq.com>
2020-07-12 11:39:15 +0800

    将代码修改为符合eslint规范

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -3,34 +3,21 @@ import {
   post,
   ajax,
 } from './ajax_axios';
-import {host} from "~/ajax/config";
+import {host} from '~/ajax/config';
 
 export const getIndex = (userName, password) => {
     return get('http://localhost:8081//page/index/index.html', { userName, password } );
 }
 
 export const doLogin = (userName, password) => {
-  return post(host+'/login', { userName, password } );
+  return post(host + '/login', { userName, password } );
 }
 
-export const prePay = (body,outTradeNo, totalFee, spbillCreateIp, notifyUrl, tradeType,signType= "MD5" ) => {
-  return post(host+'/pay/unifiedOrder',
+export const prePay = (body, outTradeNo, totalFee, spbillCreateIp, notifyUrl, tradeType, signType = 'MD5' ) => {
+  return post(host + '/pay/unifiedOrder',
     {
-      body,outTradeNo, totalFee, spbillCreateIp, notifyUrl, tradeType,signType
+      body, outTradeNo, totalFee, spbillCreateIp, notifyUrl, tradeType, signType
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -15,15 +15,15 @@ console.log('从stroage中取出的内容', umbrella.getLocalStorage('app'));
 message.success('假装这是一个持续的loading');
 
 
-doLogin('dgb','drb').then((data) => {
-    if (data.code === "0"){
+doLogin('dgb', 'drb').then((data) => {
+    if (data.code === '0'){
         message.success('请求成功');
         console.log(data);
         return ;
-    }else if(data.code=="404"){
+    }else if(data.code == '404'){
         console.log(errMsg);
     }else{
-        console.log("error!!!!!!!!!!!!!");
+        console.log('error!!!!!!!!!!!!!');
         message.error('大概是用户的错吧');
     }
 }).finally(() => {
@@ -31,19 +31,17 @@ doLogin('dgb','drb').then((data) => {
 });
 
 
+let appId, timeStamp, wx_package,
+  paySign, nonceStr;
 
-
-let appId ,timeStamp,wx_package,
-  paySign,nonceStr;
-
-//此段代码是微信支付
-prePay("since-book","123456","123",
-  "123.23.56.84","http://wxtest.easy.echosite.cn/notify/order","JSAPI").then((data) => {
-  console.log("ret ",data);
+// 此段代码是微信支付
+prePay('since-book', '123456', '123',
+  '123.23.56.84', 'http://wxtest.easy.echosite.cn/notify/order', 'JSAPI').then((data) => {
+  console.log('ret ', data);
   appId = data.appId;
-  timeStamp=data.timeStamp;
-  console.log("timestamp  ",timeStamp);
-  nonceStr=data.nonceStr;
+  timeStamp = data.timeStamp;
+  console.log('timestamp', timeStamp);
+  nonceStr = data.nonceStr;
   wx_package = data.package;
   paySign = data.sign;
 }).then(final);
@@ -52,29 +50,29 @@ prePay("since-book","123456","123",
 function onBridgeReady(){
   WeixinJSBridge.invoke(
     'getBrandWCPayRequest', {
-      "appId":appId,     //公众号名称，由商户传入
-      "timeStamp":timeStamp,         //时间戳，自1970年以来的秒数
-      "nonceStr":nonceStr, //随机串
-      "package":wx_package,
-      "signType":"MD5",         //微信签名方式：
-      "paySign":paySign //微信签名
+      'appId':appId, // 公众号名称，由商户传入
+      'timeStamp':timeStamp, // 时间戳，自1970年以来的秒数
+      'nonceStr':nonceStr, // 随机串
+      'package':wx_package,
+      'signType':'MD5', // 微信签名方式：
+      'paySign':paySign // 微信签名
     },
     function(res){
-      if(res.err_msg == "get_brand_wcpay_request:ok" ){
-        console.log("pay ok");
+      if(res.err_msg == 'get_brand_wcpay_request:ok' ){
+        console.log('pay ok');
         // 使用以上方式判断前端返回,微信团队郑重提示：
-        //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+        // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
       }
     });
 }
 
 function final() {
-  if (typeof WeixinJSBridge == "undefined"){
+  if (typeof WeixinJSBridge == 'undefined'){
     if( document.addEventListener ){
-      console.log("document.addEventListener")
+      console.log('document.addEventListener')
       document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
     }else if (document.attachEvent){
-      console.log("document.attachEvent")
+      console.log('document.attachEvent')
       document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
       document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
     }
