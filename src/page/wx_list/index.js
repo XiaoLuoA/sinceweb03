8976b43dfc6a6061254ea0dfb9a03ec5bfaea88e
dangrongjin <583365442@qq.com>
2020-07-13 15:29:24 +0800

    wxList 配送图标动态渲染

--- src/page/wx_list/index.js
+++ src/page/wx_list/index.js
@@ -1,9 +1,12 @@
+import './index.less';
 import codes from '~/config/codeConfig';
 import {getUser} from '~/ajax/user';
 import {getWXList} from '~/ajax/wxlist';
 const user = 'gdsflkghsdfoihgiosdfgdfgnghkm';
+
 getWXList(user).then((data) => {
     if (data.code === codes.success){
+        console.log(data);
         showUserList(data);
         return ;
     }else if(data.code === codes.LIST_OPENID_NOT_FOUND){
@@ -15,8 +18,17 @@ getWXList(user).then((data) => {
 });
 function showUserList(data){
     let htm='';
-    const wxList=data.data;
+	const wxList=data.data;
+	let vxListStatus='待配送';
+	let vxListStatusIcon='mui-icon-upload';
+	let vxListStatusColor='mui-badge-red';
     for(let i in data.data){
+     	if(wxList[i].status==1){
+			 vxListStatus='已送达'
+			 vxListStatusIcon='mui-icon-download'
+			 vxListStatusColor='mui-badge-green'
+		 }
+        
     htm= htm+`
     <div class="mui-card">
     <div class="mui-card-content">
@@ -27,7 +39,7 @@ function showUserList(data){
             <img class="mui-media-object mui-pull-left imgBorder" src="https://img03.sogoucdn.com/app/a/100200009/b3e8ffe1-0633-4b4f-98ce-fe4e4e8ea625.jpg">
             <!--http://www.sinceweb.xin/Image/books/${wxList[i].bookImage}-->
             <div class="mui-media-body">
-                <b>${wxList[i].bookName}</b> <span class=" mui-icon mui-icon-upload mui-pull-right mui-badge-red" style="font-size: 12px;padding: 5px;">待配送</span>
+                <b>${wxList[i].bookName}</b> <span class=" mui-icon ${vxListStatusIcon} mui-pull-right ${vxListStatusColor}" style="font-size: 12px;padding: 5px;">${vxListStatus}</span>
                 <br>
                 <small>购买数量：${wxList[i].bookNum}</small><br>
                 <small>图书价格：${wxList[i].bookPrice}元</small>
