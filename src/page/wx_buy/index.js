eb318cfbb9a430a9d3ed9ecc6c4d4eff62b2e326
Sincebook <40172250+Sincebook@users.noreply.github.com>
2020-07-13 10:08:05 +0800

    buy修改

--- src/page/wx_buy/index.js
+++ src/page/wx_buy/index.js
@@ -24,5 +24,22 @@ function changInput(isPay){
     mui.alert('请正确填写电话');
   }
 }
+let click = 1;
 const addBtn = document.getElementById('addNum');
 const delBtn = document.getElementById('delNum');
+addBtn.addEventListener('click',function(){
+  click++;
+  let price = document.getElementById('price');
+  let count = mui('#count').numbox().getValue();
+  let total = document.getElementById('total');
+  total.innerText = price.innerText * count;
+  console.log(click);
+});
+delBtn.addEventListener('click',function(){
+  click--;
+  let price = document.getElementById('price');
+  let count = mui('#count').numbox().getValue();
+  let total = document.getElementById('total');
+  total.innerText = price.innerText * count;
+  console.log(click);
+});
